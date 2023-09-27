e2e90d223d8ad466b43237e3b7e09c38a2783d68
Вечканов Алексей <avechkanov@alfabank.ru>
2023-09-27 13:36:26 +0300

    added context provider

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import { Main } from './components/main';
 import { Sidebar } from './components/sidebar';
 import { Footer } from './components/footer';
 
+import { ThemeContext } from './contexts/context';
+
 function App() {
   const [theme] = useState('light');
 
   return (
-    <div className={ `App ${theme}` }>
-      <Sidebar theme={ theme } />
-      <Main theme={ theme } />
-      <Footer />
-    </div>
+    <ThemeContext.Provider value={ theme }>
+      <div className={ `App ${theme}` }>
+        <Sidebar theme={ theme } />
+        <Main theme={ theme } />
+        <Footer />
+      </div>
+    </ThemeContext.Provider>
   );
 }
 

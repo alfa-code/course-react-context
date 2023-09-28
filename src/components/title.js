fb6b2b9d4d8051609400c1a1327498f912b062d5
Вечканов Алексей <avechkanov@alfabank.ru>
2023-09-28 14:10:53 +0300

    refiled context

--- src/components/title.js
+++ src/components/title.js
@@ -4,11 +4,19 @@ import { ThemeContext } from '../contexts/context';
 export function Title() {
     return (
         <ThemeContext.Consumer>
-            { (themeContext) => (
-                    <h1 className={ themeContext.theme }>
-                        Book List
-                    </h1>
-            ) }
+            {(upperThemeContext) => {
+                return (
+                    <ThemeContext.Provider value={{ theme: upperThemeContext.theme === 'light' ? 'dark' : 'light' }}>
+                        <ThemeContext.Consumer>
+                            {(themeContext) => (
+                                <h1 className={themeContext.theme}>
+                                    Book List
+                                </h1>
+                            )}
+                        </ThemeContext.Consumer>
+                    </ThemeContext.Provider>
+                )
+            }}
         </ThemeContext.Consumer>
     )
 }

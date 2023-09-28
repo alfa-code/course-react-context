df778676403c382cb628f10b2e74cdfec916addb
Вечканов Алексей <avechkanov@alfabank.ru>
2023-09-28 08:32:22 +0300

    universal consumer

--- src/components/title.js
+++ src/components/title.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { ThemeContext } from '../contexts/context';
 
 export function Title() {
     return (
-        <h1>
-            Book List
-        </h1>
+        <ThemeContext.Consumer>
+            { (theme) => (
+                    <h1 className={ theme }>
+                        Book List
+                    </h1>
+            ) }
+        </ThemeContext.Consumer>
     )
 }
